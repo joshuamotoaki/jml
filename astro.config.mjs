d9f433c808dbf57342238703f25c89dcd39ad345
Joshua Lau <dachshunduniverse@gmail.com>
2026-02-12 11:57:28 -0500

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://motoaki.dev",
+  markdown: {
+    shikiConfig: {
+      theme: "github-light",
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
